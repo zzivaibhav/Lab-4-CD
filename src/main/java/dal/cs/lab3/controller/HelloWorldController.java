3bb68c8b894e813c904028efe875e75e424eea65
Mehil Shah <shahmehil@dal.ca>
2024-10-01 22:14:50 -0300

    initial commit

---
+++ src/main/java/dal/cs/lab3/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package dal.cs.lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
